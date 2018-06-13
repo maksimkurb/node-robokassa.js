e68f28e1bce71da9ec98db00e258e0815fc1e6e0
Maxim Kurbatov <maksimkurb@gmail.com>
2018-06-13 20:32:41 +0400

    Linting code

--- __tests__/paymentUrl.js
+++ __tests__/paymentUrl.js
@@ -1,13 +1,15 @@
-const robokassa = require('../index');
+const robokassa = require("../index");
 
-test('payment url is generated correctly', () => {
+test("payment url is generated correctly", () => {
   const api = robokassa.create({
-    language: 'ru',
-    merchantLogin: 'demo',
-    password1: 'pa$$1',
-    password2: 'pa$$2',
-    hashingMethod: 'sha256',
+    language: "ru",
+    merchantLogin: "demo",
+    password1: "pa$$1",
+    password2: "pa$$2",
+    hashingMethod: "sha256"
   });
-  const url = api.generatePaymentUrl(156, 'Test payment. Buy \'em all!', 0);
-  expect(url).toBe('https://auth.robokassa.ru/Merchant/Index.aspx?Description=Test%20payment.%20Buy%20%27em%20all%21&InvoiceID=0&MerchantLogin=demo&OutSum=156&SignatureValue=ee90014685ca3fb72c53ee1fbe3a262e7a5430276aae9b0bea9a7caa06180a40')
+  const url = api.generatePaymentUrl(156, "Test payment. Buy 'em all!", 0);
+  expect(url).toBe(
+    "https://auth.robokassa.ru/Merchant/Index.aspx?Description=Test%20payment.%20Buy%20%27em%20all%21&InvoiceID=0&MerchantLogin=demo&OutSum=156&SignatureValue=ee90014685ca3fb72c53ee1fbe3a262e7a5430276aae9b0bea9a7caa06180a40"
+  );
 });
--- index.js
+++ index.js
@@ -1,114 +1,117 @@
 const crypto = require("crypto");
 const qs = require("query-string");
-const apisauce = require('apisauce');
-const xml2js = require('xml2js');
+const apisauce = require("apisauce");
+const xml2js = require("xml2js");
 
 const defaultConfig = {
   baseURL: "https://auth.robokassa.ru/Merchant/WebService/Service.asmx",
   paymentURL: "https://auth.robokassa.ru/Merchant/Index.aspx",
-  language: 'en',
+  language: "en",
   merchantLogin: null,
   password1: null,
   password2: null,
-  hashingMethod: 'sha256',
-  timeout: 10000,
+  hashingMethod: "sha256",
+  timeout: 10000
 };
 
 const create = (opts = defaultConfig) => {
-    const options = Object.assign({}, defaultConfig, opts);
-    if (opts.hashingMethod !== 'sha256') {
-      throw new Error('Sorry, robokassa.js supports SHA256 only hashes for now.');
-    }
-    if (!opts.merchantLogin || !opts.password1 || !opts.password2) {
-      throw new Error('Please provide merchantLogin, password1 and password2 to robokassa.js');
-    }
-    
-    const api = apisauce.create({
-      baseURL: options.baseURL,
-      timeout: options.timeout,
-    });
+  const options = Object.assign({}, defaultConfig, opts);
+  if (opts.hashingMethod !== "sha256") {
+    throw new Error("Sorry, robokassa.js supports SHA256 only hashes for now.");
+  }
+  if (!opts.merchantLogin || !opts.password1 || !opts.password2) {
+    throw new Error(
+      "Please provide merchantLogin, password1 and password2 to robokassa.js"
+    );
+  }
 
-    api.addRequestTransform(request => {
-      const { params } = request;
-      params.MerchantLogin = options.merchantLogin;
-      params.Language = options.language;
-    });
+  const api = apisauce.create({
+    baseURL: options.baseURL,
+    timeout: options.timeout
+  });
 
-    const calcHash = (payload) => {
-      return crypto
-        .createHash('sha256')
-        .update(payload)
-        .digest('hex');
-    }
+  api.addRequestTransform(request => {
+    const { params } = request;
+    params.MerchantLogin = options.merchantLogin;
+    params.Language = options.language;
+  });
 
-    /**
-     * Generate payment URL
-     * @param {number} amount Payment amount (in rubles)
-     * @param {string} description Payment description
-     * @param {any} invoiceID Invoice ID. If 0, then Robokassa will assign invoice ID automatically
-     */
-    const generatePaymentUrl = (amount, description, invoiceID = 0) => {
-      const hash = calcHash(`${options.merchantLogin}:${amount}:${invoiceID}:${options.password1}`);
-      const args = {
-        MerchantLogin: options.merchantLogin,
-        OutSum: amount,
-        InvoiceID: invoiceID,
-        Description: description,
-        Language: options.language,
-        SignatureValue: hash
-      };
-      return `${options.paymentURL}?${qs.stringify(args)}`;
-    }
+  const calcHash = payload => {
+    return crypto
+      .createHash("sha256")
+      .update(payload)
+      .digest("hex");
+  };
 
-    /**
-     * Get available currencies
-     */
-    const getCurrencies = () => api.get('/GetCurrencies');
+  /**
+   * Generate payment URL
+   * @param {number} amount Payment amount (in rubles)
+   * @param {string} description Payment description
+   * @param {any} invoiceID Invoice ID. If 0, then Robokassa will assign invoice ID automatically
+   */
+  const generatePaymentUrl = (amount, description, invoiceID = 0) => {
+    const hash = calcHash(
+      `${options.merchantLogin}:${amount}:${invoiceID}:${options.password1}`
+    );
+    const args = {
+      MerchantLogin: options.merchantLogin,
+      OutSum: amount,
+      InvoiceID: invoiceID,
+      Description: description,
+      Language: options.language,
+      SignatureValue: hash
+    };
+    return `${options.paymentURL}?${qs.stringify(args)}`;
+  };
 
-    /**
-     * Get available payment methods
-     */
-    const getPaymentMethods = () => api.get('/GetPaymentMethods');
+  /**
+   * Get available currencies
+   */
+  const getCurrencies = () => api.get("/GetCurrencies");
 
-    /**
-     * Get full amount (wanted amount + ROBOKASSA charges)
-     * @param {number} wantedAmount Wanted payment amount
-     * @param {string} currencyLabel Currency code for which amount should be calculated
-     */
-    const getAmountPlusCharges = (wantedAmount, currencyLabel = null) => api.get('/GetRates', {
+  /**
+   * Get available payment methods
+   */
+  const getPaymentMethods = () => api.get("/GetPaymentMethods");
+
+  /**
+   * Get full amount (wanted amount + ROBOKASSA charges)
+   * @param {number} wantedAmount Wanted payment amount
+   * @param {string} currencyLabel Currency code for which amount should be calculated
+   */
+  const getAmountPlusCharges = (wantedAmount, currencyLabel = null) =>
+    api.get("/GetRates", {
       IncCurrLabel: currencyLabel,
       OutSum: wantedAmount
     });
 
-    /**
-     * Get amount minus charges (payable amount - ROBOKASSA charges)
-     * @param {number} payableAmount Amount to pay by customer
-     * @param {string} currencyLabel Currency code for which amount should be calculated 
-     */
-    const getAmountMinusCharges = (payableAmount, currencyLabel = null) => api.get('/CalcOutSumm', {
+  /**
+   * Get amount minus charges (payable amount - ROBOKASSA charges)
+   * @param {number} payableAmount Amount to pay by customer
+   * @param {string} currencyLabel Currency code for which amount should be calculated
+   */
+  const getAmountMinusCharges = (payableAmount, currencyLabel = null) =>
+    api.get("/CalcOutSumm", {
       IncCurrLabel: currencyLabel,
       IncSum: payableAmount
     });
 
-    /**
-     * Get status of payment
-     * @param {any} invoiceID 
-     */
-    const getPaymentStatus = (invoiceID) => {
-      const hash = calcHash(`${options.merchantLogin}:${invoiceID}:${password2}`);
-    }
+  /**
+   * Get status of payment
+   * @param {any} invoiceID
+   */
+  const getPaymentStatus = invoiceID => {
+    const hash = calcHash(`${options.merchantLogin}:${invoiceID}:${password2}`);
+  };
 
-
-
-
-    return {
-      generatePaymentUrl,
-      getCurrencies,
-      getPaymentMethods,
-      getAmountPlusCharges,
-      getAmountMinusCharges,
-      getPaymentStatus
-    };
+  return {
+    generatePaymentUrl,
+    getCurrencies,
+    getPaymentMethods,
+    getAmountPlusCharges,
+    getAmountMinusCharges,
+    getPaymentStatus
+  };
 };
 
 module.exports = {
